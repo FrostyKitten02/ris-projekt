eedb36ce4659f4ea9ec9c5959049840807eff9e4
FrostyKitten02 <alen.fridau02@gmail.com>
2022-12-12 14:58:23 +0100

    crude operacije za naslove

--- demo/src/main/java/ris/ekipa5/demo/controllers/NaslovController.java
+++ demo/src/main/java/ris/ekipa5/demo/controllers/NaslovController.java
@@ -1,11 +1,17 @@
 package ris.ekipa5.demo.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import ris.ekipa5.demo.model.Naslov;
 import ris.ekipa5.demo.repositories.NaslovRepositroy;
+
+import java.util.Optional;
 
 
 @RestController
@@ -21,5 +27,19 @@ public class NaslovController {
         return naslovDao.findAll();
     }
 
+    @PostMapping
+    public Naslov addNaslov(@RequestBody Naslov naslov) {
+        return naslovDao.save(naslov);
+    }
+
+    @DeleteMapping("/{id}")
+    public Naslov deleteNaslo(@PathVariable long id) {
+        Optional<Naslov> naslov = naslovDao.findById(id);
+        if (naslov.isEmpty()) {
+            return null;
+        }
+        naslovDao.delete(naslov.get());
+        return naslov.get();
+    }
 
 }

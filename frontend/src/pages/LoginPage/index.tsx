e7f65f64d3c11a3e49ae47645e266031ce9413d6
FrostyKitten02 <alen.fridau02@gmail.com>
2022-12-16 15:39:30 +0100

    login inital

--- frontend/src/pages/LoginPage/index.tsx
+++ frontend/src/pages/LoginPage/index.tsx
@@ -1,6 +1,24 @@
+import React, {useState} from "react";
 
 
 export default function LoginPage() {
+    const [userName, setUsername] = useState<string>("");
+    const [password, setPassword] = useState<string>("");
 
-    return null;
+
+
+
+
+    return (
+        <div>
+            <h1>Login page</h1>
+
+            <label>Username</label>
+            <input id="username" name="username"></input>
+
+            <label>Username</label>
+            <input id="username" name="username"></input>
+
+        </ div>
+    )
 }
